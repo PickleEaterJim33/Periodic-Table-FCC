af9f0ff7716c15e33c743dbbbb2ca21d763cc444
PickleEaterJim33 <jan.mlinaric@gmail.com>
2022-10-28 15:40:38 +0000

    feat: add types table

--- element.sh
+++ element.sh
@@ -22,9 +22,11 @@ fi
 
 echo "$ELEMENT" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME
 do
-  PROPERTIES=$($PSQL "SELECT atomic_number, type, ROUND(atomic_mass, 3), melting_point_celsius, boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-  echo "$PROPERTIES" | while read ATOMIC_MUMBER BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
+  PROPERTIES=$($PSQL "SELECT atomic_number, ROUND(atomic_mass, 3), melting_point_celsius, boiling_point_celsius, type_id FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+  echo "$PROPERTIES" | while read ATOMIC_MUMBER BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS BAR TYPE_ID
   do
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. Hydrogen has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+    TYPE=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID")
+    TYPE_FORMATTED=$(echo "$TYPE" | sed -r 's/^ *| *$//g')
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE_FORMATTED, with a mass of $ATOMIC_MASS amu. Hydrogen has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
   done
 done
