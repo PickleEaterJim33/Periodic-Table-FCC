e4a25aea60b2d49b6cd03abb1f5eb17e84fb1f7d
PickleEaterJim33 <jan.mlinaric@gmail.com>
2022-10-28 15:05:05 +0000

    refactor: rename columns

--- element.sh
+++ element.sh
@@ -22,9 +22,9 @@ fi
 
 echo "$ELEMENT" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME
 do
-  PROPERTIES=$($PSQL "SELECT atomic_number, type, ROUND(weight, 3), melting_point, boiling_point FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-  echo "$PROPERTIES" | while read ATOMIC_MUMBER BAR TYPE BAR WEIGHT BAR MELTING_POINT BAR BOILING_POINT
+  PROPERTIES=$($PSQL "SELECT atomic_number, type, ROUND(atomic_mass, 3), melting_point_celsius, boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+  echo "$PROPERTIES" | while read ATOMIC_MUMBER BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT
   do
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $WEIGHT amu. Hydrogen has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. Hydrogen has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
   done
 done
